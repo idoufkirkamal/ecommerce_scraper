dfed6fa1a6e04eb80b89324fc4f1e4aac7ed7148
ouchgout Mohamed <91564786+ouchgoutmohamed@users.noreply.github.com>
2025-01-22 14:56:13 +0100

    adding scraping data

--- src/scraping/amazon/ComputergraphicscardsScraping.py
+++ src/scraping/amazon/ComputergraphicscardsScraping.py
@@ -7,7 +7,7 @@ import os
 from datetime import datetime
 
 # Constants
-BASE_OUTPUT_DIR = r"C:\Users\AdMin\Desktop\ecommerce_scraper\data\raw"
+BASE_OUTPUT_DIR = r"/data/raw"
 USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
